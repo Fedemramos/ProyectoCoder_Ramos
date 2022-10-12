f6c0c4c7a4939324677b28e99c274409fc304ed8
fede2025 <ramosfederico76@gmail.com>
2022-10-11 21:26:02 -0300

    Modelos

---
+++ ProyectofinalCoder/Appblog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Autor(models.Model):
+    class Meta:
+        verbose_name_plural = "Autores"
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    profesion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre, self.apellido
+
+
+class Seccion(models.Model):
+    class Meta:
+        verbose_name_plural = "Secciones"
+
+    clase = models.CharField(max_length=150)
+    fecha = models.CharField(max_length=150)
+
+
+class Articulo(models.Model):
+
+    titulo = models.CharField(max_length=150)
+    texto = models.CharField(max_length=150)
+    fecha = models.CharField(max_length=150)
